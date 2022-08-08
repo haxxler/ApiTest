9417e2bdf738984abeb013f70f9a984b2f5beae7
haxxler <haxxkim@gmail.com>
2022-08-08 10:42:07 +0900

    Project initialization

---
+++ main.js
@@ -0,0 +1,20 @@
+// arithemetic operating
+// + - * / %
+
+// assignment operating
+// a += 1은 사용가능 파이썬은 안됨
+// 일치 연산 a === 1
+
+// ternary operation
+import randTest from './getRandom';
+import typeChck from './getType';
+
+
+let a = 1;
+let b = 4;
+console.log(a, b, a === b);
+b = a;
+console.log(a, b, a === b);
+a = 7;
+console.log(a, b, a === b);
+let c = 1;
